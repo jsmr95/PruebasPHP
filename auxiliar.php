0801198226e9a2ddfc69b0599d1c649ac48b94b1
Jose Maria Gallego <josemaria.gallego@iesdonana.org>
2018-11-02 11:36:18 +0100

    Creo la sentencia de busqueda por genero. Y meto todas las busquedas en auxiliar, en la funcion compruebaBusquedas()

--- auxiliar.php
+++ auxiliar.php
@@ -11,4 +11,34 @@
     $st->execute(['id' => $id]);
     return $st->fetch();
   }
-  
+
+  function compruebaBuscadores($pdo,$buscarArticulo,$buscarMarca,$buscarGenero){
+    if ($buscarGenero != '') {
+    $st = $pdo->prepare('SELECT p.*, genero
+                        FROM productos p
+                        JOIN generos g
+                        ON genero_id = g.id
+                        WHERE position(lower(:articulo) in lower(articulo)) != 0
+                        AND position(lower(:marca) in lower(marca)) != 0
+                        AND position(lower(:genero) in lower(genero)) != 0');
+    $st->execute([':articulo' => "$buscarArticulo", ':marca' => "$buscarMarca", ":genero" => "$buscarGenero"]);
+    } elseif($buscarMarca != '') {
+      $st = $pdo->prepare('SELECT p.*, genero
+                          FROM productos p
+                          JOIN generos g
+                          ON genero_id = g.id
+                          WHERE position(lower(:articulo) in lower(articulo)) != 0
+                          AND position(lower(:marca) in lower(marca)) != 0');
+      $st->execute([':articulo' => "$buscarArticulo", ':marca' => "$buscarMarca"]);
+    } else {
+      $st = $pdo->prepare('SELECT p.*, genero
+                          FROM productos p
+                          JOIN generos g
+                          ON genero_id = g.id
+                          WHERE position(lower(:articulo) in lower(articulo)) != 0'); //position es como mb_substrpos() de php, devuelve 0
+                                                                                  //si no encuentra nada. ponemos lower() de postgre para
+                                                                                  //que no distinga entre mayu y minus
+      $st->execute([':articulo' => "$buscarArticulo"]);
+    }
+    return $st;
+  }
--- index.php
+++ index.php
@@ -41,26 +41,8 @@
           $buscarGenero = isset($_GET['buscarGenero'])
                           ? trim($_GET['buscarGenero'])
                           : '';
-          if ($buscarMarca != '') {
-            $st = $pdo->prepare('SELECT p.*, genero
-                                FROM productos p
-                                JOIN generos g
-                                ON genero_id = g.id
-                                WHERE position(lower(:articulo) in lower(articulo)) != 0
-                                AND position(lower(:marca) in lower(marca)) != 0'); //position es como mb_substrpos() de php, devuelve 0
-                                                                                        //si no encuentra nada. ponemos lower() de postgre para
-                                                                                        //que no distinga entre mayu y minus
-            $st->execute([':articulo' => "$buscarArticulo", ':marca' => "$buscarMarca"]);
-          } else {
-          $st = $pdo->prepare('SELECT p.*, genero
-                              FROM productos p
-                              JOIN generos g
-                              ON genero_id = g.id
-                              WHERE position(lower(:articulo) in lower(articulo)) != 0'); //position es como mb_substrpos() de php, devuelve 0
-                                                                                      //si no encuentra nada. ponemos lower() de postgre para
-                                                                                      //que no distinga entre mayu y minus
-          $st->execute([':articulo' => "$buscarArticulo"]);
-        }
+          $st = compruebaBuscadores($pdo,$buscarArticulo,$buscarMarca,$buscarGenero);
+
           ?>
         </div>
           <div class="row" id="busqueda">
