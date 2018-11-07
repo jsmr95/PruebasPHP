e3b4455ce3e9a7bc665ed9eb7bdbc1b009588fe2
Jose Maria Gallego <josemaria.gallego@iesdonana.org>
2018-11-07 12:16:51 +0100

    Añado una linea al final de index.php

--- index.php
+++ index.php
@@ -110,7 +110,7 @@
           <a href="insertar.php" class="btn btn-primary">Insertar articulo nuevo</a>
         </div>
       </div>
-    </div>
+    </div><br>
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
     </body>
