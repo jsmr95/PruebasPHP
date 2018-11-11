1d2ade5c775c6b2ca772e9248c8b761b2e6d3641
Jose Maria Gallego <josemaria.gallego@iesdonana.org>
2018-11-11 13:19:58 +0100

    Primera opcion de ventana Bienvenido

--- index.php
+++ index.php
@@ -8,7 +8,33 @@
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
   </head>
   <body>
-
+    <?php
+    require './auxiliar.php';
+    ?>
+    <nav class="navbar navbar-default">
+    <div class="container-fluid">
+          <!-- Collect the nav links, forms, and other content for toggling -->
+      <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+        <a class="navbar-brand" href="#">Bienvenido!</a>
+        <a class="navbar-brand" href="#">Informatica</a>
+      </div><!-- /.navbar-collapse -->
+    </div><!-- /.container-fluid -->
+    </nav>
+    <div class="container">
+      <div class="row">
+        <div class="col-md-12">
+          <div class="panel panel-default">
+            <div class="panel-heading">
+              <h4 align="center">Bienvenido!</h4>
+            </div>
+            <div class="panel-body">
+              <pre>Aqui podrás encontrar todo los articulos que desees.
+Espero que lo encuentres en alguna de nuestras diferentes secciones.</pre> <br>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
     </body>
